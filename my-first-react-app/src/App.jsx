0fb7be20eb3a228c7c5d93c3bc8189d7d0640214
Luiz Felipe Ribeiro Souza <lipezerafar@gmail.com>
2025-08-26 08:19:50 -0300

    feat: Adição de componentes e props

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,32 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Components
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+
+      <h2>Hello World!</h2>
+
+      <Card title="Shrek" rating={5} isCool={true} actors={[{name: "Di Caprio"}]}/> 
+      <Card title="The Lion King" rating={3} isCool={false}/>
+      <Card title="Demon Slayer" rating={5} isCool={true}/>
+
+    </div>
+
+  )
+}
+
+const Card = ({title}) => {
+  return (
+    <div>
+
+      <h2>{title}</h2>
+
+    </div>
   )
 }
 
