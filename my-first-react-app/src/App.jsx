7238fbecf3c14940be26156faa60903b2a60d176
luizfeliperibs <lipezerafar@gmail.com>
2025-08-26 15:57:30 -0300

    feat: Aplicação de hooks como UseEffect e UseState

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 // Components
+// Properties
+// UseEffects | UseState
 
 const App = () => {
   return (
 
     <div>
 
-      <h2>Hello World!</h2>
-
-      <Card title="Shrek" rating={5} isCool={true} actors={[{name: "Di Caprio"}]}/> 
-      <Card title="The Lion King" rating={3} isCool={false}/>
-      <Card title="Demon Slayer" rating={5} isCool={true}/>
+      <Card title="Shrek" rating={5} isCool={true}/> 
+      <Card title="The Lion King"/>
+      <Card title="Demon Slayer"/>
 
     </div>
 
@@ -22,10 +22,30 @@ const App = () => {
 }
 
 const Card = ({title}) => {
+
+  const [count, setCount] = useState(0);
+
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked]);
+
+  useEffect(() => {
+    console.log('Card rendered')
+  }, []);
+
   return (
-    <div>
+
+    <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
 
       <h2>{title}</h2>
+
+      <h2>{count || null}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+          {hasLiked ? 'Liked' : 'Like'}
+      </button>
 
     </div>
   )
