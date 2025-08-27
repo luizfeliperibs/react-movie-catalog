9de2ef4960b1d65ab72aba1fa042f8ff31644a58
Luiz Felipe Ribeiro Souza <lipezerafar@gmail.com>
2025-08-27 11:13:20 -0300

    feat: Implementação da API TMDB - V1

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,9 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Search from './components/Search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+      try {
+        
+        const endpoint = `${API_BASE_URL}/discover/movie/sort_by=popularity.desc`;
+
+        const response = await fetch(endpoint, API_OPTIONS);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch movies');
+        }
+
+      } catch (error) {
+        console.error(`Error fetching movies:, ${error}`);
+      }
+  }
+    
+
+  useEffect(() => {
+
+  }, []);
 
   return (
 
@@ -16,17 +51,20 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero-Banner"/>
           <h1>Busque por <span className="text-gradient">Filmes</span> De Todo Lugar!</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-
-        <h1>{searchTerm}</h1>
+        <section className="all-movies">
+          <h2>Todos Os Filmes</h2>
+        </section>
 
       </div>
+
     </main>
 
   )
 
 }
+
 
 export default App
--- my-first-react-app/src/components/Search.jsx
+++ my-first-react-app/src/components/Search.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Search = ({searchTerm, setSearchTerm}) => {
 
     return(
+        
         <div className="search">
             <div>
                 <img src="search.svg" alt="search"/>
